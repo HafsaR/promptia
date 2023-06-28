9e1bdc32602017538f4bf4c5f78eb8a1c87bf24c
HafsaR <hafsa34343@gmail.com>
2023-06-27 23:59:38 -0600

    Added delete prompt function

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -6,17 +6,28 @@ import { useState, useEffect, useReducer } from "react";
 import Profile from "@components/profile";
 
 const MyProfile = () => {
-
   const router = useRouter();
 
-  const {data: session} = useSession();
-  const [posts, setPosts] = useState([])
+  const { data: session } = useSession();
+  const [posts, setPosts] = useState([]);
 
   const handleEdit = (post) => {
-    router.push(`/update-prompt?id=${post._id}`)
+    router.push(`/update-prompt?id=${post._id}`);
   };
 
-  const handleDelete = async () => {};
+  const handleDelete = async (post) => {
+    const hasConfirm = confirm("Are you sure you want to delete this prompt?");
+    if (hasConfirm) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: "DELETE",
+        });
+        const filteredPosts = posts.filter((p) => p._id !== post._id);
+
+        setPosts(filteredPosts);
+      } catch (error) {}
+    }
+  };
 
   useEffect(() => {
     const fetchPost = async () => {
@@ -24,7 +35,7 @@ const MyProfile = () => {
       const data = await response.json();
       setPosts(data);
     };
-   if(session?.user.id) fetchPost();
+    if (session?.user.id) fetchPost();
   }, []);
 
   return (
